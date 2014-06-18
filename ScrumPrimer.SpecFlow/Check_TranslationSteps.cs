037c4132ad144b6e950a289d4583556630449e48
Steven Mak <stevenmak@gmail.com>
2014-06-19 01:25:33 +0800

    Initial Commit

---
+++ ScrumPrimer.SpecFlow/Check_TranslationSteps.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+using FluentAutomation;
+
+namespace ScrumPrimer.SpecFlow
+{
+    [Binding]
+    public class Check_TranslationSteps : FluentTest
+    {
+        public Check_TranslationSteps()
+        {
+            SeleniumWebDriver.Bootstrap( SeleniumWebDriver.Browser.Chrome);
+        }
+
+        private string baseURL;
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            baseURL = "http://scrumprimer.org";
+        }
+
+        [Given(@"I am on the home page")]
+        public void GivenIAmOnTheHomePage()
+        {
+            I.Open(baseURL);
+        }
+
+        [When(@"I go to scrum primer overview translation page")]
+        public void WhenIGoToScrumPrimerOverviewTranslationPage()
+        {
+            I.Assert.Exists("#navTranslations");
+            I.Click("a[href='/translations']");
+        }
+
+        [Then(@"I will see scrum primer overview in '(.*)'")]
+        public void ThenIWillSeeScrumPrimerOverviewIn(string language)
+        {
+            Assert.AreEqual("http://scrumprimer.org/overview/" + language + "_scrum_overview.png", I.Find("img[id='overview_translated']").Element.Attributes.Get("src"));
+        }
+    }
+}
---
+++ ScrumPrimer.SpecFlow/check_translation.feature
@@ -0,0 +1,10 @@
+﻿Feature: check_translation
+	In order to read a scrum primer
+	As a foreign people
+	I want to see scrum primer in my own language
+
+@mytag
+Scenario: Default translation language
+	Given I am on the home page
+	When I go to scrum primer overview translation page
+	Then I will see scrum primer overview in 'zh-cn'
